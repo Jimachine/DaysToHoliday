35efaa7603976f16a966d1b761de863f0e831473
Jimachine <jimmy_onion@hotmail.com>
2019-04-29 08:46:59 +0100

    first commit

---
+++ DaysToHoliday/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysToHoliday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime holidayStart = new DateTime(2019, 8, 26);
+            DateTime today = DateTime.Now;
+
+            TimeSpan daysToGo = holidayStart.Date - today.Date;
+
+            
+            Console.WriteLine(daysToGo.ToString("dddd").TrimStart('0'));
+        }
+    }
+}
